5c18f8504a1528152e78bbf7175cc480b74096a7
kellymhli <55807683+kellymhli@users.noreply.github.com>
2019-10-07 19:54:15 -0700

    Add function to prompt user to add more melon info. Add new info to existing melon dictionary."

--- melon_info.py
+++ melon_info.py
@@ -9,7 +9,7 @@ def print_melon(name, seedless, price, flesh_color, rind_color, avg_weight):
 
     print(f"""{name.upper()}"""
           f"""\n\tseedless: {seedless}"""
-          f"""\n\tprice: {price}"""
+          f"""\n\tprice: {price:.2f}"""
           f"""\n\tflesh_color: {flesh_color}"""
           f"""\n\tweight: {avg_weight}"""
           f"""\n\trind_color: {rind_color}"""
@@ -17,29 +17,28 @@ def print_melon(name, seedless, price, flesh_color, rind_color, avg_weight):
 
 
 def add_info(melon_info_dict):
+    """Add melon information to melon_info_dictionary."""
 
+    # Prompt user for info to add to melon dictionary.
     melon = input("What melon would you like to add info to? ").title()
-    info = input(f"What kind of info would you like to add to {melon}? ")
+    info = input(f"What kind of info would you like to add to {melon}? ").lower()
     value = input(f"What is the value for {info}? ")
 
+    # Add melon info to dictionary.
     melon_info_dict[melon][info] = value
-    return (melon_info_dict)
 
 
 def main():
 
-    for melon, info in melon_info_dict.items():
-        price = info['price']
-        seedless = info['seedless']
-        flesh_color = info['flesh_color']
-        rind_color = info['rind_color']
-        avg_weight = info['avg_weight']
-        print_melon(melon, seedless, price, flesh_color, rind_color, avg_weight)
-
-    while input("Would you like to add new melon info? (Y/N) ") != 'N':
+    # Promp user if they want to add new info to the melon dictionary.
+    while input("Would you like to add new melon info? (Y/N) ").upper() != 'N':
         add_info(melon_info_dict)
 
-
+    # Print out each melon and its formation.
+    for melon, info in melon_info_dict.items():
+        print(melon.upper())
+        for key, value in info.items():
+            print(f'\t{key}: {value}')
 
 
 main()
