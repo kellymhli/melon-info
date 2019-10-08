309ed4e0fa9253d3b047bfdc4ed05459682e297b
kellymhli <55807683+kellymhli@users.noreply.github.com>
2019-10-07 19:21:03 -0700

    Change melon_info.py functions so it prints all info."

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,36 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices, melon_characteristics
+from melons import melon_info
 
 
-def print_melon(name, seedless, price):
+def print_melon(name, seedless, price, flesh_color, rind_color, avg_weight):
     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+    # have_or_have_not = 'have'
+    # if seedless:
+    #     have_or_have_not = 'do not have'
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+    # print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+
+    print(f"""{name.upper()}"""
+          f"""\n\tseedless: {seedless}"""
+          f"""\n\tprice: {price}"""
+          f"""\n\tflesh_color: {flesh_color}"""
+          f"""\n\tweight: {avg_weight}"""
+          f"""\n\trind_color: {rind_color}"""
+         )
 
 
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+def main():
+
+    for melon, info in melon_info.items():
+        price = info['price']
+        seedless = info['seedless']
+        flesh_color = info['flesh_color']
+        rind_color = info['rind_color']
+        avg_weight = info['avg_weight']
+        print_melon(melon, seedless, price, flesh_color, rind_color, avg_weight) 
+
+
+main()
--- melons.py
+++ melons.py
@@ -1,51 +1,27 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
-
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
-
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
-
-melon_characteristics = {
+melon_info = {
     'Honeydew': {'price': 0.99, 
                  'seedless': True,
-                 'flesh color': 'orange',
-                 'rind color': 'None',
-                 'average weight': 'None'}
+                 'flesh_color': 'orange',
+                 'rind_color': 'None',
+                 'avg_weight': 'None'},
     'Crenshaw': {'price': 2.00,
                  'seedless': False,
-                 'flesh color': 'orange',
-                 'rind color': 'None',
-                 'average weight': 'None'}
+                 'flesh_color': 'orange',
+                 'rind_color': 'None',
+                 'avg_weight': 'None'},
     'Crane': {'price': 2.50,
               'seedless': False,
-              'flesh color': 'orange',
-              'rind color': 'None',
-              'average weight': 'None'}
+              'flesh_color': 'orange',
+              'rind_color': 'None',
+              'avg_weight': 'None'},
     'Casaba': {'price': 2.50,
                'seedless': False,
-               'flesh color': 'white',
-               'rind color': 'None',
-               'average weight': 'None'}
+               'flesh_color': 'white',
+               'rind_color': 'None',
+               'avg_weight': 'None'},
     'Cantaloupe': {'price': 0.99,
                    'seedless': False,
-                   'flesh color': 'orange',
-                   'rind color': 'None',
-                   'average weight': 'None'}
+                   'flesh_color': 'orange',
+                   'rind_color': 'None',
+                   'avg_weight': 'None'}
 }
