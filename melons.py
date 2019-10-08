41c495a00985b1576de27a35392598b4b818da4d
kellymhli <55807683+kellymhli@users.noreply.github.com>
2019-10-07 18:54:04 -0700

    Added nested dictionary in melons.py to track info for each melon.

---
+++ melons.py
@@ -0,0 +1,51 @@
+melon_names = {
+    1: 'Honeydew',
+    2: 'Crenshaw',
+    3: 'Crane',
+    4: 'Casaba',
+    5: 'Cantaloupe',
+}
+
+melon_prices = {
+    1: 0.99,
+    2: 2.00,
+    3: 2.50,
+    4: 2.50,
+    5: 0.99,
+}
+
+melon_seedlessness = {
+    1: True,
+    2: False,
+    3: False,
+    4: False,
+    5: False,
+}
+
+melon_characteristics = {
+    'Honeydew': {'price': 0.99, 
+                 'seedless': True,
+                 'flesh color': 'orange',
+                 'rind color': 'None',
+                 'average weight': 'None'}
+    'Crenshaw': {'price': 2.00,
+                 'seedless': False,
+                 'flesh color': 'orange',
+                 'rind color': 'None',
+                 'average weight': 'None'}
+    'Crane': {'price': 2.50,
+              'seedless': False,
+              'flesh color': 'orange',
+              'rind color': 'None',
+              'average weight': 'None'}
+    'Casaba': {'price': 2.50,
+               'seedless': False,
+               'flesh color': 'white',
+               'rind color': 'None',
+               'average weight': 'None'}
+    'Cantaloupe': {'price': 0.99,
+                   'seedless': False,
+                   'flesh color': 'orange',
+                   'rind color': 'None',
+                   'average weight': 'None'}
+}
