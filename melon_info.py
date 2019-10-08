0e0b4280fd1c460ba43dadab80c20ac2619cbf2c
kellymhli <55807683+kellymhli@users.noreply.github.com>
2019-10-07 19:33:25 -0700

    Change melon_info.py functions so it prints all info."
    slear

--- melon_info.py
+++ melon_info.py
@@ -1,17 +1,11 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_info
+from melons import melon_info_dict
 
 
 def print_melon(name, seedless, price, flesh_color, rind_color, avg_weight):
     """Print each melon with corresponding attribute information."""
-
-    # have_or_have_not = 'have'
-    # if seedless:
-    #     have_or_have_not = 'do not have'
-
-    # print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
 
     print(f"""{name.upper()}"""
           f"""\n\tseedless: {seedless}"""
@@ -22,15 +16,30 @@ def print_melon(name, seedless, price, flesh_color, rind_color, avg_weight):
          )
 
 
+def add_info(melon_info_dict):
+
+    melon = input("What melon would you like to add info to? ").title()
+    info = input(f"What kind of info would you like to add to {melon}? ")
+    value = input(f"What is the value for {info}? ")
+
+    melon_info_dict[melon][info] = value
+    return (melon_info_dict)
+
+
 def main():
 
-    for melon, info in melon_info.items():
+    for melon, info in melon_info_dict.items():
         price = info['price']
         seedless = info['seedless']
         flesh_color = info['flesh_color']
         rind_color = info['rind_color']
         avg_weight = info['avg_weight']
-        print_melon(melon, seedless, price, flesh_color, rind_color, avg_weight) 
+        print_melon(melon, seedless, price, flesh_color, rind_color, avg_weight)
+
+    while input("Would you like to add new melon info? (Y/N) ") != 'N':
+        add_info(melon_info_dict)
+
+
 
 
 main()
--- melons.py
+++ melons.py
@@ -1,24 +1,29 @@
-melon_info = {
+# Nested dictionary of melon info.
+melon_info_dict = {
     'Honeydew': {'price': 0.99, 
                  'seedless': True,
                  'flesh_color': 'orange',
                  'rind_color': 'None',
                  'avg_weight': 'None'},
+
     'Crenshaw': {'price': 2.00,
                  'seedless': False,
                  'flesh_color': 'orange',
                  'rind_color': 'None',
                  'avg_weight': 'None'},
+
     'Crane': {'price': 2.50,
               'seedless': False,
               'flesh_color': 'orange',
               'rind_color': 'None',
               'avg_weight': 'None'},
+
     'Casaba': {'price': 2.50,
                'seedless': False,
                'flesh_color': 'white',
                'rind_color': 'None',
                'avg_weight': 'None'},
+
     'Cantaloupe': {'price': 0.99,
                    'seedless': False,
                    'flesh_color': 'orange',
